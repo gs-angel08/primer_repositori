add4733f56d902a96b37491df9291430e4ec8b8a
gs-angel08 <guerrerosotoangeldavid@gmail.com>
2024-10-21 09:31:04 -0600

    Add files via upload

---
+++ P00Presentacion.php
@@ -0,0 +1,13 @@
+<?php
+/* CBTIS 89 
+   Programa que contiene mis datos personales
+   Angel Guerrerp
+   3ºA Programaciòn Matutino
+   */
+
+   echo "Mi nombre es Angel David Guerrero Soto";
+   echo "<br>","Estudio en el CBTIS 89";
+   echo "<br>","En 3er semestre de la carrera de Programaciòn A Matutino";
+   echo "<br>","Vivo en la cuidad de Durango,Dgo";
+
+ ?>  
